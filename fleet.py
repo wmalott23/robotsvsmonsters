77866d27f57666945383644c6066407d67bd1c3b
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-03-21 11:29:31 -0700

    finished fleet.py

--- fleet.py
+++ fleet.py
@@ -5,4 +5,9 @@ class Fleet:
         self.robots = []
 
     def create_fleet(self):
-        pass
+        robot_one = input("Name your first robot to enter the fleet")
+        robot_two = input("Name a second robot to enter your fleet")
+        robot_three = input("Name a third robot to enter your fleet")
+        self.robots.append(Robot(robot_one), Robot(robot_two), Robot(robot_three))
+        print(f'Your robot fleet is {self.robots}')
+

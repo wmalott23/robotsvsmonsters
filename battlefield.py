1fbeac78ea84b8d210949ba8f39eafd4f9232fa0
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-03-21 11:09:02 -0700

    mapped battlefield.py

--- battlefield.py
+++ battlefield.py
@@ -3,4 +3,29 @@ from herd import Herd
 
 class Battlefield:
     def __init__(self):
-        self.fleet = 
+        self.fleet = self.fleet.robots
+        self.herd = self.herd.dinosaurs
+
+    def run_game():
+        pass
+
+    def display_welcome():
+        pass
+
+    def battle():
+        pass
+
+    def dino_turn():
+        pass
+
+    def robo_turn():
+        pass
+
+    def show_dino_opponent_options(self):
+        pass
+
+    def show_robo_opponent_options(self):
+        pass
+
+    def display_winners(self):
+        pass

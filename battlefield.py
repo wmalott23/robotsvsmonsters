f9b101d9a1888b8b877b84094b74d2a1d8ed7043
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-03-21 13:25:27 -0700

    Made considerable progress, but still troubleshooting battle logic method in battlefield.py

--- battlefield.py
+++ battlefield.py
@@ -10,22 +10,49 @@ class Battlefield:
         pass
 
     def display_welcome(self):
-        pass
+        print("Welcome to the big battle of dinosaurs and robots!")
 
     def battle(self):
-        pass
+            counter = 1
+            if counter % 2 == 0:
+                self.show_dino_opponent_options()
+                robo_choice = input("Which robot would like to attack with?")
+                for robo_choice in self.fleet.robots:
+                    self.robo_turn(robo_choice)
+            elif counter % 2 != 0:
+                self.show_robo_opponent_options()
+                dino_choice = input("Which dino would you like to attack with?")
+                for dino_choice in self.herd.dinosaurs:
+                    self.dino_turn(dino_choice)
+            counter += 1
 
     def dino_turn(self, dinosaur):
-        pass
+        self.show_dino_opponent_options()
+        att_choice = input("Which robo would you like to attack?")
+        for att_choice in self.fleet.robots:
+            att_choice.health -= dinosaur.attack_power
+        dinosaur.attack()
 
     def robo_turn(self, robot):
-        pass
+        self.show_robo_opponent_options()
+        att_choice = input("Which dino would you like to attack?")
+        for att_choice in self.herd.dinosaurs:
+            att_choice.health -= robot.attack_power
+        robot.attack()
 
     def show_dino_opponent_options(self):
-        pass
+        for each in self.fleet.robots:
+            if each.health >= 0:
+                print(each.name)
 
     def show_robo_opponent_options(self):
-        pass
+        for each in self.herd.dinosaurs:
+            if each.health >= 0:
+                print(each.name)
 
     def display_winners(self):
         pass
+
+herd = Battlefield()
+
+herd.battle()
--- dinosaur.py
+++ dinosaur.py
@@ -1,7 +1,7 @@
 class Dinosaur:
     def __init__(self, name, attack_power):
         self.name = name
-        self.attack_power = attack_power
+        self.attack_power = int(attack_power)
         self.health = 50
 
     def attack(self, robot):

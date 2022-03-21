1b83da6ca422b4cca45c138b065ef59190dc44b3
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-03-21 14:39:22 -0700

    Added weapons to the robots

--- robot.py
+++ robot.py
@@ -4,8 +4,22 @@ class Robot:
     def __init__(self, name):
         self.name = name
         self.health = 40
-        self.weapon = Weapon("sword", 40)
+        self.weapon = Weapon("Error", 404)
+        self.weapon.list = [Weapon("Space Sword", 40), Weapon("Space Laser", 30), Weapon("Space Punch", 35)]
 
 
     def attack(self, dinosaur):
+        self.choose_weapon()
         print(f'{self.name} attacks {dinosaur} with their {self.weapon.name} for {self.weapon.attack_power} points of damage!')
+    
+    def choose_weapon(self):
+        for each in self.weapon.list:
+            print(each.name)
+        wep_choice = input("What weapon would you like to use?")
+        for each in self.weapon.list:
+            if each.name == wep_choice:
+                self.weapon = each
+
+robert = Robot("robert")
+
+robert.attack("godzilla")

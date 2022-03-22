176423595bc28e2806e8377cceb5a0360efaccab
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-03-22 08:08:12 -0700

    Added logic to prevent accidental incorrect move choices

--- dinosaur.py
+++ dinosaur.py
@@ -4,7 +4,7 @@ class Dinosaur:
         self.attack_power = int(attack_power)
         self.health = 50
         self.energy = 100
-        self.att_list = ["Tail whip", "Body Slam", "Stomp", "Hyper Beam"]
+        self.att_list = ["Tail Whip", "Body Slam", "Stomp", "Hyper Beam"]
         self.att = "error"
 
     def attack(self, robot):
@@ -12,9 +12,12 @@ class Dinosaur:
         print(f'{self.name} attacks {robot} with a {self.att} and does {self.attack_power} points of damage!')
 
     def att_choose(self):
-        for each in self.att_list:
-            print(each)
-        attack_choice = input("What attack would you like to use?")
-        for each in self.att_list:
-            if each == attack_choice:
-                self.att = each
+        choice = 0
+        while choice != 1:
+            for each in self.att_list:
+                print(each)
+            attack_choice = input("What attack would you like to use?")
+            for each in self.att_list:
+                if each == attack_choice:
+                    self.att = each
+                    choice = 1
